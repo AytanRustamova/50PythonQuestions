095ed3ae09836e674cc1ba2bc7312568be46fa58
AytanRustamova <aytanrustamzada@gmail.com>
2021-11-26 11:30:01 +0300

    format string enumerate method to improve code

---
+++ test.py
@@ -0,0 +1,5 @@
+import string
+for index , letter in enumerate(string.ascii_letters):
+    print(f'The letter at index {index} is {letter}')
+    
+# 1. Enumerate methodu iterable'lərin sayını özündə saxladığına görə itarableləri artırmaq kimi işlərə ehtiyac duyulmur. 
